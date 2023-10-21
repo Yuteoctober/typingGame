9215680105f3dc98820cf946bb5dffd194af628f
YuteOctober <yudthsoponvit@gmail.com>
2023-10-21 07:43:35 -0400

    update my link to my portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -357,7 +357,7 @@ function App() {
         </div>
         <div className="right_btm">
           <BsFillHouseHeartFill className='port'/>
-          <a href='https://yute-dev.netlify.app/'> Check out my Portfolio</a>
+          <a href='https://yuteoctober.github.io/dndPortfolio/'> Check out my Portfolio</a>
         </div>
       </div>
       )}
